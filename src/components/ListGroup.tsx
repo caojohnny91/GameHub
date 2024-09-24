f654c56403c9cfaa7adc0b16eb1b9a392eea8e6e
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-09-24 14:24:26 -0500

    create conditional rendering of list if no items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  // in return you cannot write javascript unless it has {}, only html and react components
   return (
     <>
       <h1>Title</h1>
+      {items.length === 0 && <p>No items found</p>}
+
       <ul className="list-group">
         {/* cant use for loop so need to use .map with arrow function and wrap entire thin in {}, and each li needs a unique key */}
         {items.map((item) => (
-          <li key={item}> {item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
