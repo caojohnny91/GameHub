819a9488a0a4c3fc15e6d8b8caa72d44990b2f72
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-09-24 15:25:23 -0500

    use props to pass data to components

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
-
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
+// useing typescript to set datatype { items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+}
+// getting the props from App.tsx to use here
+function ListGroup(props: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1); // default state to -1 so no li is selected
 
   // in return you cannot write javascript unless it has {}, only html and react components
   return (
     <>
-      <h1>Title</h1>
-      {items.length === 0 && <p>No items found</p>}
+      <h1>{props.heading}</h1>
+      {props.items.length === 0 && <p>No items found</p>}
 
       <ul className="list-group">
         {/* cant use for loop so need to use .map with arrow function and wrap entire thin in {}, and each li needs a unique key */}
-        {items.map((item, index) => (
+        {props.items.map((item, index) => (
           <li
             // add active class to selected li
             className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
