b519d226e2b87f425c436172033c7acfc86bc052
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-09-24 13:21:48 -0500

    init commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+// PascalCasing: Capitalized first letter
+function Message() {
+  // JSX: JavaScript XML
+  const name = "Johnny";
+  if (name) {
+    return <h1>Hello {name}</h1>;
+  } else {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default Message;
