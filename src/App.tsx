8ae91cead13e8b4e3cee3fe590b0bbbaccb70c68
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-09-26 13:32:05 -0500

    create alert propmt when button is clicked and create functionality to close alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 import { Alert } from "./components/Alert";
 import { Button } from "./components/Button";
 
@@ -9,15 +10,24 @@ function App() {
   //   console.log(item);
   // };
 
+  // create useState for when button is clicked to make alert visible
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
       {/* <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} /> */}
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+
+      {/* wrapping in {} to perform logic, using && to make first condition false */}
+      {/* add the onClose prop and create annoymous arrow function */}
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>This is my ALERT! </Alert>}
 
       {/* adding color now changes the className in button component  */}
-      <Button color="danger" onClick={() => console.log("Clicked!")}>
+      {/* <Button color="danger" onClick={() => console.log("Clicked!")}>
+        My Button!
+      </Button> */}
+
+      {/* updating onClick function from console.log to setAlertVisibility to true so that we can see the alert popup */}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>
         My Button!
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,7 +3,15 @@ import { ReactNode } from "react";
 // using interface Prop to set the Prop data types and model
 interface Props {
   children: ReactNode; // using children instead of text to mak eit easier in app.tsx
+  onClose: () => void; // create prop and set to void so that we can creat functionality on app,tsx
 }
-export const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+export const Alert = ({ children, onClose }: Props) => {
+  return (
+    // add className and button from bootstrap
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}></button>
+      {/* call the onClose funcitonality when the close button is clicked */}
+    </div>
+  );
 };
